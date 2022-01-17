4de3a5cb4720a7d0a32e769dbfebdc33fb870e89
MesutRuzgar <mesutruzgar@gmail.com>
2022-01-17 14:47:00 +0300

    404 hatası

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    //[ApiController]
     public class ProductsController : ControllerBase
     {
         [HttpGet]
