2232954a8095819e2fc946ecd4891d97ea4d440f
MesutRuzgar <mesutruzgar@gmail.com>
2022-01-20 17:29:34 +0300

    14.ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde ürün mevcut.";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string UserRegistered = "Kullanıcı kayıt oldu";
+        public static string PasswordError = "Hatalı şifre";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "";
     }
 }
